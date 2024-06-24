7d7e902bc181447800f2ac91cc2c9994d21243f9
David Ramlakhan <dram1964@gmail.com>
2024-06-24 16:25:51 +0100

    Added config module

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,17 @@ from app import app
 from flask import render_template
 
 @app.route("/")
+@app.route("/index")
 def home():
     user = {"username": "David"}
-    return render_template('index.html', title="Informus", user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Informus", user=user, posts=posts)
