2783c02490d6ba2e153b82d53ce31c330916bc10
David Ramlakhan <dram1964@gmail.com>
2024-06-25 18:07:52 +0100

    Added some basic form entry

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
 def home():
     user = {"username": "David"}
+    print(f"{url_for('home')} called")
     posts = [
         {
             'author': {'username': 'John'},
@@ -16,3 +18,13 @@ def home():
         }
     ]
     return render_template('index.html', title="Informus", user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+    print(f"{url_for('login')} called")
+    return render_template('login.html', title='Login', form=form )
